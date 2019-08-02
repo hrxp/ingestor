3a3693900acf712f05557f157e5e4fa1d30f8ebb
wizard-jS <spraguej624@gmail.com>
2019-08-01 20:53:06 -0700

    Transfer files from the hrxp/api repo

---
+++ ingestor.js
@@ -0,0 +1,84 @@
+// TODOs
+// We should add the unzipper somewhere in here as well
+// Find out if we can get the relative file path instead of absolute
+// Next step after we've recursed over the archive would be to insert all the parsed data into the data base, aligning data with our mongoDB data model
+
+const fs = require('fs');
+const path = require('path');
+const { processFileController } = require('./utils.js');
+
+// Local State
+let state = {};
+state.results = [];
+state.users = [];
+state.channels = [];
+state.messages = [];
+
+const filewalker = async (dir, done) => {
+  // Read the directory (unzipped archive), this is also the recursive function.
+  await fs.readdir(dir, async function(err, list) {
+    // If at any point there is an error, invoke the callback function with the error as the argument
+    if (err) return done(err);
+
+    // Check to see if there are any items in the list
+    var pending = list.length;
+
+    // base case # 2, if there are no more items in the list, return
+    if (!pending) return done(null, users, messages, channels);
+
+    // For each item in the file list, iterate over the file.
+    list.forEach(file => {
+      // Save the relative path of a file in this variable
+      let relativeFilePath = file;
+
+      // Grab the absolute path of a file
+      file = path.resolve(dir, file);
+
+      // Grab information about a file
+      fs.stat(file, async (err, stat) => {
+        // If there are stats and there is a directory for this file(folder), we will push the file/folder into the results array
+        if (stat && stat.isDirectory()) {
+          state.results.push(file);
+
+          // If we reach here, we know this file is a directory, and we recuseively call filewalker with the directory
+          filewalker(file, async (err, res) => {
+            state.results = state.results.concat(res);
+            if (!--pending) done(null);
+          });
+        } else {
+          // /*
+          //  ** Here is where we would read each file and parse the data, there are 4 different cases
+          //  */
+
+          // There are 4 different cases for a file, find the case and insert into the correct array
+          if (relativeFilePath === 'users.json') {
+            let user = await processFileController(file);
+            state.users.push(user);
+          } else if (relativeFilePath === 'channels.json') {
+            let channelResults = await processFileController(file);
+            state.channels.push(channelResults);
+          } else if ((relativeFilePath = 'integration_logs')) {
+            // We are not keeping track of this data?
+          } else {
+            const results = await processFileController(file);
+            state.messages.push(results);
+          }
+          // results.push(file) not sure if we need to add the file to the list here?
+
+          // If there are more items in the list, invoke the callback
+          if (!--pending) {
+            done(null);
+          }
+        }
+      });
+    });
+  });
+};
+
+// Invoke the file walker function
+filewalker(path.resolve('./ingestor'), err => {
+  if (err) {
+    throw err;
+  }
+  console.log(state.users);
+});
---
+++ utils.js
@@ -0,0 +1,64 @@
+// TODO
+// await use axios post seedData fn to push data to db
+// done: delete file directory(or add to gitignore so zip files doesn’t push to Github) + drain
+
+const fs = require('fs');
+const path = require('path');
+const unzipper = require('unzipper'); // npm lib used to unzip files
+const { createReadStream } = require('fs');
+
+// async fs reads and unzip files to a directory
+
+const folderName = 'Archive.zip';
+const pathToExtract = 'unzippedArchive';
+
+const unzipFolder = (folderName, pathToExtract) => {
+  fs.createReadStream(`./${folderName}`).pipe(
+    unzipper.Extract({ path: __dirname + `/${pathToExtract}` })
+  );
+};
+
+class File {
+  constructor(filename) {
+    this.filename = filename;
+    this.fileContents;
+  }
+
+  readFile() {
+    return new Promise((resolve, reject) => {
+      try {
+        fs.createReadStream(__dirname + '/channels.json')
+
+          .on('data', function(chunk) {
+            this.fileContents = JSON.parse(chunk.toString());
+          })
+
+          .on('close', function() {
+            resolve(this.fileContents);
+          });
+      } catch (err) {
+        reject();
+      }
+    });
+  }
+
+  async processFile(processFileFunction, ...otherArgs) {
+    const results = processFileFunction(...otherArgs);
+    return results;
+  }
+}
+
+const processFileController = async (fileName, func, funcArgs) => {
+  let state = {};
+  state.readFile = new File(fileName);
+
+  try {
+    // Read the file and store in the File object
+    const results = await state.readFile.readFile();
+    return results;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = { File, processFileController };
