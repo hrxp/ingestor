9655293adff1b0c9523f1a9781d06a0b595553e7
miken619 <miken619@gmail.com>
2019-08-04 17:06:31 -0700

    Update File.js
    Update the `File` class into a Singleton.

--- src/File.js
+++ src/File.js
@@ -1,32 +1,57 @@
 const fs = require('fs');
 
-class File {
-  constructor(filename) {
-    this.filename = filename;
+export const DEFAULT_FILENAME = 'some string'; // TODO: Filled me in.
+
+class FileUtils {
+  constructor() {
     this.fileContents;
+    this.setFileContents(DEFAULT_FILENAME);
   }
 
-  // Read a file and save into the constructor
-  getFileContents() {
+  // Read a file and saved its contents into fileContents.
+  __setFileContents(filename) {
     return new Promise((resolve, reject) => {
-      fs.createReadStream(this.filename)
+      fs.createReadStream(filename)
         .on('data', chunk => {
           this.fileContents = JSON.parse(chunk.toString());
         })
-        .on('error', err => {
-          reject(err);
-        })
         .on('close', () => {
           resolve(this.fileContents);
+        })
+        .on('error', err => {
+          reject(err);
         });
     });
   }
 
-  // This method is here in the case we wanted to do invoke a function on the this.fileContents
-  async processFile(processFileFunction, ...otherArgs) {
-    const results = processFileFunction(...otherArgs);
-    return results;
-  }
+ getFileContents() {
+   return this.fileContents;
+ }
+  
+ setFileContents(filename) {
+   const setFileContentsPromise = this.__setFileContents(filename);
+   setFileContentsPromise
+     .then((fileContents) => this.fileContents = fileContents)
+     .catch((err) => {
+       // TODO: Handle error
+     });
+ }
+  
+ formatUsers(users) {
+   // TODO: Format the inputted Users data.  
+ }
+  
+ formatChannels(channels) {
+   // TODO: Format the inputted Channels data.  
+ }
+  
+ formatMessagess(messages) {
+   // TODO: Format the inputted Messages data.  
+ }
 }
 
-module.exports = { File };
+// Reading and parsing from a file is an expensive operation. To ensured the `__setFileContents` method does not
+// get needlessly run we export a Singleton.
+const fileUtils = new FileUtils();
+
+module.exports = { fileUtils };
