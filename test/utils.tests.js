ba0727961bfa50207ebd06e500471be55de1d9ff
wizard-jS <spraguej624@gmail.com>
2019-08-08 12:06:31 -0700

    Refactor tests for the utils file

--- test/utils.tests.js
+++ test/utils.tests.js
@@ -1,5 +1,4 @@
 const { processFileController } = require('../src/utils.js');
-const File = require('../src/File').File;
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 
@@ -9,33 +8,9 @@ chai.use(chaiAsPromised);
 const testFilePath = `${__dirname}/testFile.json`;
 const invalidFilePath = './invalidPath';
 
-describe('File Class', () => {
-  const newFile = new File(testFilePath);
-
-  it('it should be an object that containts a filename property', () => {
-    expect(newFile.filename).to.be.equal(testFilePath);
-    //Testing a returned promise
-  });
-
-  it('it should read a file and parse the json data and store in the filecontents property', async () => {
-    const results = await newFile.getFileContents();
-    expect(results)
-      .to.be.an('object')
-      .to.have.property('test');
-    expect(results.test).to.equal('testMessage');
-  });
-
-  it('it should catch an error if the file path is incorrect', async () => {
-    const newFile = new File(invalidFilePath);
-    await expect(newFile.getFileContents()).to.be.rejected;
-  });
-});
-describe('fileProcessController Utility', () => {
+describe('FileProcessController utility', () => {
   it("it should pass in a valid file path and output the file's contentes", async () => {
     const results = await processFileController(testFilePath);
-    expect(results)
-      .to.be.an('object')
-      .to.have.property('test');
     expect(results.test).to.equal('testMessage');
   });
 
