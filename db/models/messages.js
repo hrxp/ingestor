af21ac691474ae550097934a7982a03868634c3f
wizard-jS <spraguej624@gmail.com>
2019-08-14 15:33:27 -0700

    Add updated messages model

---
+++ db/models/messages.js
@@ -0,0 +1,40 @@
+// TODOS
+// Figure out how to reference anoter schema in Mongoose
+// Explore populate to use with the reference https://mongoosejs.com/docs/populate.html
+// TODO: user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  user: String,
+  ts: String,
+  type: String,
+  text: String,
+  channelName: String,
+  files: [{ id: String, displayName: String, fileType: String, downloadUrl: String }],
+  replies: [
+    {
+      user: String,
+      ts: String,
+      type: String,
+      ChannelName: String,
+      text: String,
+      files: [{ id: String, displayName: String, fileType: String, downloadUrl: String }],
+    },
+  ],
+});
+
+// Create an instance (document) of the messageSchema
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = {
+  insertMessages: async channels => {
+    try {
+      for (let messages in channels) {
+        await Message.collection.insertMany(channels[messages]);
+      }
+      return;
+    } catch (err) {
+      return err;
+    }
+  },
+};
